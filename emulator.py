7e9941a038dff0a4a2c992ab4e85bfedcc7dc2a3
Keanne <miguelking293@gmail.com>
2025-06-05 22:14:36 +0800

    Bug Fixes

--- emulator.py
+++ emulator.py
@@ -17,14 +17,6 @@ class Arch242Emulator:
         self.TEMP = 0
         self.IOA = 0x0
         
-        self.REG = {
-            0 : self.RA,
-            1 : self.RB,
-            2 : self.RC,
-            3 : self.RD,
-            4 : self.RE
-        }
-    
         self.instructions = instructions
         
     def step(self):
@@ -43,31 +35,51 @@ class Arch242Emulator:
         print(self.PC)
         
         # REG INSTRUCTIONS (check 4 leftmost == 0b0001 and bits 2-4 <= 4 (4 registers))
-        if (instruction >> 4 == 0x01) and (instruction & 0x0E >> 1 <= 4):
+        if ((instruction >> 4) == 0x01) and (((instruction & 0x0E) >> 1) <= 4):
             # inc*-reg
-            if instruction & 0x01 == 0b0:
-                reg = instruction & 0x0E >> 1
+            if (instruction & 0x01) == 0b0:
+                reg = (instruction & 0x0E) >> 1
                 if 0 <= reg <= 4:
-                    self.REG[reg] += 1
+                    if reg == 0: self.RA += 1
+                    elif reg == 1: self.RB += 1
+                    elif reg == 2: self.RC += 1
+                    elif reg == 3: self.RD += 1
+                    elif reg == 4: self.RE += 1
             
             # dec*-reg
-            elif instruction & 0x01 == 0b1:
-                reg = instruction & 0x0E >> 1
+            elif (instruction & 0x01) == 0b1:
+                reg = (instruction & 0x0E) >> 1
                 if 0 <= reg <= 4:
-                    self.REG[reg] -= 1                
+                    if reg == 0: self.RA -= 1
+                    elif reg == 1: self.RB -= 1
+                    elif reg == 2: self.RC -= 1
+                    elif reg == 3: self.RD -= 1
+                    elif reg == 4: self.RE -= 1    
+                    
+            self.step()           
 
 
-        # REG to/from ACC INSTRUCTIONS (4 leftmost == 0b0010 and bits 2-4 >= 4)
-        elif (instruction >> 4 == 0b0010) and (instruction & 0x0E >> 1 <= 4):
+        # REG to/from ACC INSTRUCTIONS (4 leftmost == 0b0010 and bits 2-4 <= 4)
+        elif ((instruction >> 4) == 0b0010) and (((instruction & 0x0E) >> 1) <= 4):
             # to-reg
-            if (instruction & 0x01 == 0):
-                reg = instruction & 0x0E >> 1
-                self.REG[reg] = self.ACC
+            if ((instruction & 0x01) == 0):
+                reg = (instruction & 0x0E) >> 1
+                if reg == 0: self.RA = self.ACC
+                elif reg == 1: self.RB = self.ACC
+                elif reg == 2: self.RC = self.ACC
+                elif reg == 3: self.RD = self.ACC
+                elif reg == 4: self.RE = self.ACC
             
             # from-reg
-            elif (instruction & 0x01 == 1):
-                reg = instruction & 0x0E >> 1
-                self.ACC = self.REG[reg]
+            elif ((instruction & 0x01) == 1):
+                reg = (instruction & 0x0E) >> 1
+                if reg == 0: self.ACC = self.RA
+                elif reg == 1: self.ACC = self.RB
+                elif reg == 2: self.ACC = self.RC
+                elif reg == 3: self.ACC = self.RD
+                elif reg == 4: self.ACC = self.RE
+                
+            self.step()
 
 
         # IMMEDIATE OPERATIONS
@@ -95,12 +107,14 @@ class Arch242Emulator:
             # or
             elif instruction==0x44:
                 immediate=self.instructions[self.PC+1]
-                self.ACC^=immediate
+                self.ACC|=immediate
 
             # r4
             elif instruction==0x46:
                 immediate=self.instructions[self.PC+1]
-                self.REG[4]=immediate
+                self.RE=immediate
+                
+            self.step()
 
 
         # IMMEDIATE ASSIGNMENT
@@ -123,6 +137,8 @@ class Arch242Emulator:
             elif (instruction>>4==0b0111):
                 immediate=instruction&0x0F
                 self.ACC=immediate
+                
+            self.step()
         
         
         #b-bit  
@@ -131,60 +147,77 @@ class Arch242Emulator:
             immediate= (instruction&0x07)<<8|self.instructions[self.PC+1]
             if self.ACC>>k:
                 self.PC=(self.PC&0xF800)|immediate
+            else:
+                self.step()
         
         
         # IMMEDIATE BRANCH INSTRUCTIONS
         elif 0b10100 <= instruction>>3 <= 0b11011:
             #bnz-a
             if(instruction>>3==0b10100):
-                immediate= (instruction&0x07)<<8|self.instructions[self.PC+1]
+                immediate= ((instruction&0x07)<<8)|self.instructions[self.PC+1]
                 if self.RA!=0:
                     self.PC=(self.PC&0xF800)|immediate
+                else:
+                    self.step()
             
             #bnz-B
             elif(instruction>>3==0b10101):
-                immediate= (instruction&0x07)<<8|self.instructions[self.PC+1]
+                immediate= ((instruction&0x07)<<8)|self.instructions[self.PC+1]
                 if self.RB!=0:
                     self.PC=(self.PC&0xF800)|immediate
+                else:
+                    self.step()
             
             #beqz
             elif(instruction>>3==0b10110):
-                immediate= (instruction&0x07)<<8|self.instructions[self.PC+1]
-                if self.ACC!=0:
+                immediate= ((instruction&0x07)<<8)|self.instructions[self.PC+1]
+                if self.ACC==0:
                     self.PC=(self.PC&0xF800)|immediate
+                else:
+                    self.step()
             
             #bnez
             elif(instruction>>3==0b10111):
-                immediate= (instruction&0x07)<<8|self.instructions[self.PC+1]
-                if self.ACC==0:
+                immediate= ((instruction&0x07)<<8)|self.instructions[self.PC+1]
+                if self.ACC!=0:
                     self.PC=(self.PC&0xF800)|immediate
+                else:
+                    self.step()
             
             #beqz-cf
             elif(instruction>>3==0b11000):
-                immediate= (instruction&0x07)<<8|self.instructions[self.PC+1]
+                immediate= ((instruction&0x07)<<8)|self.instructions[self.PC+1]
                 if self.CF==0:
                     self.PC=(self.PC&0xF800)|immediate
+                else:
+                    self.step()
             
             #beqz-cf
             elif(instruction>>3==0b11001):
-                immediate= (instruction&0x07)<<8|self.instructions[self.PC+1]
+                immediate= ((instruction&0x07)<<8)|self.instructions[self.PC+1]
                 if self.CF!=0:
                     self.PC=(self.PC&0xF800)|immediate
+                else:
+                    self.step()
             
             #bnz-d
             elif(instruction>>3==0b11011):
-                immediate= (instruction&0x07)<<8|self.instructions[self.PC+1]
+                immediate= ((instruction&0x07)<<8)|self.instructions[self.PC+1]
                 if self.RD!=0:
                     self.PC=(self.PC&0xF800)|immediate
+                else:
+                    self.step()
         
         #b
         elif(instruction>>4==0b1110):
-            immediate= (instruction&0x0F)<<8|self.instructions[self.PC+1]
+            immediate= ((instruction&0x0F)<<8)|self.instructions[self.PC+1]
             self.PC=(self.PC&0xF000)|immediate
         
         #call
         elif(instruction>>4==0b1111):
-            immediate= (instruction&0x0F)<<8|self.instructions[self.PC+1]
+            self.TEMP = self.PC + 2
+            immediate= ((instruction&0x0F)<<8)|self.instructions[self.PC+1]
             self.PC=(self.PC&0xF000)|immediate
             
         
@@ -192,23 +225,23 @@ class Arch242Emulator:
         elif 0x00 <= instruction <= 0x0F:
             # rot-r
             if instruction == 0x00:
-                self.ACC = (self.ACC << 3 & 0x0F) | (self.ACC >> 1)
+                self.ACC = ((self.ACC << 3) & 0x0F) | (self.ACC >> 1)
             
             # rot-l
             elif instruction == 0x01:
-                self.ACC = (self.ACC >> 3) | (self.ACC << 1 & 0x0F)
+                self.ACC = (self.ACC >> 3) | ((self.ACC << 1) & 0x0F)
             
             # rot-rc
             elif instruction == 0x02:
                 cfacc = (self.CF << 4) | self.ACC
-                cfacc = (cfacc << 4 & 0x1F) | (cfacc >> 1)
+                cfacc = ((cfacc << 4) & 0x1F) | (cfacc >> 1)
                 self.CF = cfacc >> 4
                 self.ACC = cfacc & 0x0F
             
             # rot-lc
             elif instruction == 0x03:
                 cfacc = (self.CF << 4) | self.ACC
-                cfacc = (cfacc >> 4) | (cfacc << 1 & 0x1F)
+                cfacc = (cfacc >> 4) | ((cfacc << 1) & 0x1F)
                 self.CF = cfacc >> 4
                 self.ACC = cfacc & 0x0F
             
@@ -230,14 +263,14 @@ class Arch242Emulator:
             # to-mdc
             elif instruction == 0x07:
                 dc = (self.RD << 4) | self.RC
-                self.memory[dc] = self.memory[dc]
+                self.memory[dc] = self.ACC
             
             # addc-mba
             elif instruction == 0x08:
                 ba = (self.RB << 4) | self.RA
                 acc = self.ACC + self.memory[ba] + self.CF
                 if acc >> 4:
-                    self.CF = acc >> 4 & 0x01
+                    self.CF = (acc >> 4) & 0x01
                     self.ACC = acc & 0x0F
                 else:
                     self.ACC = acc
@@ -247,7 +280,7 @@ class Arch242Emulator:
                 ba = (self.RB << 4) | self.RA
                 acc = self.ACC + self.memory[ba]
                 if acc >> 4:
-                    self.CF = acc >> 4
+                    self.CF = (acc >> 4) & 0x01
                     self.ACC = acc & 0x0F
                 else:
                     self.ACC = acc
@@ -258,7 +291,7 @@ class Arch242Emulator:
                 val = self.ACC - self.memory[ba] + self.CF
                 if val < 0:
                     self.CF = 1
-                self.ACC = val
+                self.ACC = val & 0x0F
                 
             # sub-mba
             elif instruction == 0x0B:
@@ -266,7 +299,7 @@ class Arch242Emulator:
                 val = self.ACC - self.memory[ba]
                 if val < 0:
                     self.CF = 1
-                self.ACC = val
+                self.ACC = val & 0x0F
                 
             # inc*-mba
             elif instruction == 0x0C:
@@ -287,6 +320,8 @@ class Arch242Emulator:
             elif instruction == 0x0F:
                 dc = (self.RD << 4) | self.RC
                 self.memory[dc] -= 1
+                
+            self.step()
 
 
         # LOGICAL OPERATIONS (check 4 leftmost == 0b0001)
@@ -294,35 +329,37 @@ class Arch242Emulator:
             # and-ba
             if instruction == 0x1A:
                 ba = (self.RB << 4) | self.RA
-                self.ACC = self.ACC & self.memory[ba]
+                self.ACC &= self.memory[ba]
                 
             # xor-ba
             elif instruction == 0x1B:
                 ba = (self.RB << 4) | self.RA
-                self.ACC = self.ACC ^ self.memory[ba]
+                self.ACC ^= self.memory[ba]
             
             # or-ba
             elif instruction == 0x1C:
                 ba = (self.RB << 4) | self.RA
-                self.ACC = self.ACC | self.memory[ba]
+                self.ACC |= self.memory[ba]
                 
             # and*-mba
             elif instruction == 0x1D:
                 ba = (self.RB << 4) | self.RA
-                self.memory[ba] = self.ACC & self.memory[ba]
+                self.memory[ba] &= self.ACC
                 
             # xor*-mba
             elif instruction == 0x1E:
                 ba = (self.RB << 4) | self.RA
-                self.memory[ba] = self.ACC ^ self.memory[ba]
+                self.memory[ba] ^= self.ACC
                 
             # or*-mba
             elif instruction == 0x1F:
                 ba = (self.RB << 4) | self.RA
-                self.memory[ba] = self.ACC | self.memory[ba]
+                self.memory[ba] |= self.ACC
+                
+            self.step()
         
         
-        elif 0x2A <= instruction <= 0x2E:
+        elif 0x2A <= instruction <= 0x2B:
             # clr-cf    
             if instruction==0x2A:
                 self.CF=0
@@ -330,48 +367,51 @@ class Arch242Emulator:
             # set-cf
             elif instruction==0x2B:
                 self.CF=1
+                
+            self.step()
             
-            # ret
-            elif instruction==0x2E:
-                self.PC=(self.PC&0x1000)|self.TEMP&0x0111
-                self.TEMP=0
-
+        # ret
+        elif instruction==0x2E:
+            self.PC=(self.PC&0xF000)|(self.TEMP&0x0FFF)
+            self.TEMP=0
+        
 
         elif 0x32 <= instruction <= 0x3F:
             # from-ioa
             if instruction==0x32:
                 self.ACC=self.IOA
-            
+                self.step()
+                
             # inc
             elif instruction==0x31:
                 self.ACC+=1
-            
+                self.step()
+                
             # bcd
             elif instruction==0x36:
                 if self.ACC>=10 or self.CF:
                     self.ACC+=6
                     self.CF=1
-            
+                self.step()
+                
             # shutdown
             elif instruction==0x37:
                 exit()
             
             # nop
             elif instruction==0x3E:
-                ...
+                self.step()
             
             # dec
             elif instruction==0x3F:
                 self.ACC-=1
-        
+                self.step()
+                
       
         #invalid instruction from .byte
         else:
             print("Invalid Instruction!")
             exit()
-
-        self.step()
-        
 
 
 #############
